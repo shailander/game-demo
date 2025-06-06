30625f830db1d13b96c2e456ece18257b09236d7
shailander <shailander@livelike.com>
2025-06-06 18:05:26 +0530

    feature: added new game sports-center

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import TriviaShowdown from "./game/trivia-showdown";
 import ESPNSportsMillionaire from "./game/millionaire-trivia";
 import ESPNPersonalityShowdown from "./game/personality-showdown";
+import SportsCenter from "./game/sports-center";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           path="personality-showdown"
           element={<ESPNPersonalityShowdown />}
         />
+        <Route path="/sports-center" element={<SportsCenter />} />
         <Route path="*" element={<Navigate to="/Trivia_Showdown" replace />} />
       </Routes>
     </div>
---
+++ src/game/sports-center/index.jsx
@@ -0,0 +1,12 @@
+export default function SportsCenter() {
+  return (
+    <iframe
+      className="h-screen w-screen"
+      src="/sports-center.html"
+      title="Sports Center"
+      width="100%"
+      height="100%"
+      style={{ border: "none", minHeight: "500px" }}
+    />
+  );
+}
