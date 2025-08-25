b29787972dd658c6750c2e199039181cf4eff96a
shailander <shailander@livelike.com>
2025-08-25 10:37:04 +0530

    feat(bingo-cricket): new game added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import BingoCricket from "./game/bingo-cricket";
 import FoxMillionaireTrivia from "./game/fox-millionaire-trivia";
 import FoxTriviaShowdown from "./game/fox-trivia-showdown";
 import ESPNSportsMillionaire from "./game/millionaire-trivia";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/sports-center" element={<SportsCenter />} />
         <Route path="/fox-trivia-showdown" element={<FoxTriviaShowdown />} />
         <Route path="/fox-millionaire-trivia" element={<FoxMillionaireTrivia />} />
+        <Route path="/bingo-cricket" element={<BingoCricket />} />
         <Route path="*" element={<Navigate to="/Trivia_Showdown" replace />} />
       </Routes>
     </div>
---
+++ src/game/bingo-cricket/index.jsx
@@ -0,0 +1,14 @@
+export default function BingoCricket() {
+    console.log("HI")
+    return (
+      <iframe
+        className="h-screen w-screen"
+        src="/bingo-cricket.html"
+        title="Bingo Cricket"
+        width="100%"
+        height="100%"
+        style={{ border: "none", minHeight: "500px" }}
+      />
+    );
+  }
+  
